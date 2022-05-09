ab0175b89e41a2324022dddcc362655ea30f678d
Claire Lin <claire.lin10@gmail.com>
2022-05-09 22:38:02 +0800

    add selection function

--- read.py
+++ read.py
@@ -15,3 +15,9 @@ sum_len = 0
 for d in data:
     sum_len += len(d)
 print('留言長度平均為', sum_len/len(data))
+#篩選, 寫程式的難處在把要做的事情邏輯化，並用正確的程式碼撰寫出來。
+new = []
+for d in data:
+    if len(d) > 300:
+        new.append(d)
+print('總共有', len(new), '筆資料留言數字大於300')
