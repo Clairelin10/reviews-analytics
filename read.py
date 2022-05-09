f121b403d24d76fde4ec006da604d20d53754d14
Claire Lin <claire.lin10@gmail.com>
2022-05-09 22:27:54 +0800

    cal avg

--- read.py
+++ read.py
@@ -1,6 +1,17 @@
 data =[]
+count = 0
 with open ('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
+        count += 1 # count = count + 1
+        if count % 50000 == 0:
+            print(len(data))
+print ('檔案讀取完畢，總共有',len(data),'筆資料。')
 
-print (data[2], data[5])
+
+print(data[0])
+print(len(data[0]))
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print('留言長度平均為', sum_len/len(data))
